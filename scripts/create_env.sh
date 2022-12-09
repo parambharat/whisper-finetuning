d28d90c75f4896d2b28f415c52d32f8e7c63f50f
Bharat Ramanathan <ramanathan.parameshwaran@gohuddl.com>
2022-12-09 10:59:04 +0530

    ops: add env creation script

---
+++ scripts/create_env.sh
@@ -0,0 +1,30 @@
+#create env
+conda create -n whisper_finetuning python=3.9
+conda activate whisper_finetuning
+#add apt packages
+add-apt-repository -y ppa:jonathonf/ffmpeg-4
+apt update
+apt install -y ffmpeg git git-lfs
+# install conda packages
+conda install -y jupyter scikit-learn numpy pandas matplotlib
+conda install -y -c pyviz holoviews bokeh
+conda install -y -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
+mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+conda install -y pytorch torchvision torchaudio pytorch-cuda=11.6 -c pytorch -c nvidia
+
+#install pip packages
+python -m pip install -U pip
+python -m pip install -U tensorflow
+
+python -m pip install librosa \
+soundfile \
+git+https://github.com/huggingface/datasets \
+git+https://github.com/huggingface/transformers \
+"evaluate>=0.3.0" \
+jiwer \
+more-itertools \
+"wandb>=0.13.6" \
+bitsandbytes \
+audiomentations \
+pydub
